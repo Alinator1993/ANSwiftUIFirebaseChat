0de5acf868f52ac288203f3f07f1829a383fe641
Allan Ntaate <alnnte1@gmail.com>
2023-09-25 16:43:56 -0500

    Initial Commit

---
+++ ANSwiftUIFirebaseChat/ANSwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ANSwiftUIFirebaseChatApp.swift
+//  ANSwiftUIFirebaseChat
+//
+//  Created by Allan Ntaate on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ANSwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
